2c33515b7c415e90c561359b27d3d68fbd936f0c
Keenah <kvancamp@mail.com>
2023-05-25 15:48:21 -0500

    added footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary defaultKeyword="dictionary" />
+        </main>
+        <footer>
+          This project was coded by Keenah VanCampenhout and is hosted on{" "}
+          <a
+            href="https://github.com/clarsongit/dictionary-project"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
 export default App;
