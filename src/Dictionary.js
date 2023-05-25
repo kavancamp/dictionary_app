ba33a2e30d9bd6b12f545e00929efbdba3e6b79d
Keenah <kvancamp@mail.com>
2023-05-25 18:10:07 -0500

    added api/response

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert("Searching");
+    let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(url).then(handleResponse);
+    console.log(url);
   }
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
