fedca3c3f924425b715e96403e258a90cab6d5f2
Keenah <kvancamp@mail.com>
2023-05-25 20:54:44 -0500

    Update Result.js

--- src/Result.js
+++ src/Result.js
@@ -1,13 +1,34 @@
 import React from "react";
-import './Result.css'
+import "./Result.css";
 import Definition from "./Definition";
 
 export default function Result(props) {
+  function utterance(event) {
+    let vocate = new SpeechSynthesisUtterance(`${props.results.word}`);
+    speechSynthesis.speak(vocate);
+  }
   console.log(props.meaning);
   if (props.results) {
     return (
-      <div className="Results">
-        <h2>{props.results.word}</h2>
+      <div className="Result">
+        <section>
+          <h2>{props.results.word}</h2>
+
+          <div>
+            <button 
+              href="/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="sound"
+              onClick={utterance}
+            >
+              Listen <i className="fa-solid fa-volume-high ms-1"></i>
+            </button>{" "}
+            {"   "}
+            <span className="text">{props.results.phonetic} </span>
+          </div>
+        </section>
+
         {props.results.meanings.map(function (meaning, index) {
           return (
             <section key={index}>
