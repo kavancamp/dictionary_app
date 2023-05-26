c19fc688502afc5bceed25582e6a9eb913f87936
Keenah <kvancamp@mail.com>
2023-05-25 19:28:41 -0500

    added states

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
+import Result from "./Result";
+import "./Dictionary.css";
 import axios from "axios";
 import "./Dictionary.css";
+
 export default function Dictionary() {
-  let [keyword, setKeyword] = useState("");
+  const [keyword, setKeyword] = useState("");
+  const [results, setResults] = useState(null);
+  const [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
-    console.log(response.data);
+    console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function search(event) {
@@ -17,11 +23,35 @@ export default function Dictionary() {
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
-  return (
-    <div className="dictionary">
-      <form onSubmit={search}>
-        <input type="search" onChange={handleKeywordChange} />
-      </form>
-    </div>
+  function available() {
+    setLoaded(true);
+    search();
+  }
+  const searchForm = (
+    <form onSubmit={search}>
+      <input
+        type="search"
+        placeholder="Search for a word"
+        onChange={handleKeywordChange}
+      />
+    </form>
   );
+
+  if (available) {
+    return (
+      <div className="Dictionary">
+        <h2>📖</h2>
+        <h1>What word would you like to learn?</h1>
+        {searchForm}
+        <Result results={results} />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h3>Sorry- No data available for this term</h3>
+        {searchForm}
+      </div>
+    );
+  }
 }
